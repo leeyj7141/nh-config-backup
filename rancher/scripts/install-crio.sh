f8f25582d79bfb9b1afe2b27571a4fed680cd03f
Youngju Lee <yjlee@linux.com>
2024-07-16 06:01:52 +0000

    ADD: rancher helm chart added

---
+++ rancher/scripts/install-crio.sh
@@ -0,0 +1,7 @@
+OS=CentOS_8
+VERSION=1.28
+sudo curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable.repo https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/devel:kubic:libcontainers:stable.repo
+sudo curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.repo https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/devel:kubic:libcontainers:stable:cri-o:$VERSION.repo
+sudo dnf install -y cri-o
+sudo systemctl enable crio --now
+
