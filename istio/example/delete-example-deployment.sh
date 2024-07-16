c90476e70c5aea6538b678fad4ed36d720bf2b7c
Youngju Lee <yjlee@linux.com>
2024-07-16 06:04:15 +0000

    ADD: istio example deployment added

---
+++ istio/example/delete-example-deployment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+
+export MYHOST=$(kubectl config view -o jsonpath={.contexts..namespace}).bookinfo.com
+
+kubectl delete -l version!=v2,version!=v3 -f https://raw.githubusercontent.com/istio/istio/release-1.22/samples/bookinfo/platform/kube/bookinfo.yaml
+
+kubectl delete -f https://raw.githubusercontent.com/istio/istio/release-1.22/samples/sleep/sleep.yaml
+
+#kubectl exec $(kubectl get pod -l app=sleep -o jsonpath='{.items[0].metadata.name}') -c sleep -- curl -sS productpage:9080/productpage | grep -o "<title>.*</title>"
+
