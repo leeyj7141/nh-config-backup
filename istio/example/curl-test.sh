6d9b7b8b976d34f646ba4c14b19be215d9c97c01
Youngju Lee <yjlee@linux.com>
2024-07-18 07:15:14 +0000

    CLEANUP: file cleanup

---
+++ istio/example/curl-test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+
+#export MYHOST=$(kubectl config view -o jsonpath={.contexts..namespace}).bookinfo.com
+#
+#kubectl apply -l version!=v2,version!=v3 -f https://raw.githubusercontent.com/istio/istio/release-1.22/samples/bookinfo/platform/kube/bookinfo.yaml
+#
+#kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.22/samples/sleep/sleep.yaml
+
+kubectl exec $(kubectl get pod -l app=sleep -o jsonpath='{.items[0].metadata.name}') -c sleep -- curl -sS productpage:9080/productpage | grep -o "<title>.*</title>"
