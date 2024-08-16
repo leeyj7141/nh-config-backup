f96414baf19e7261431d2e4df294557e36ec92b3
Youngju Lee <yjlee@linux.com>
2024-08-16 07:32:17 +0000

    NOTE: istio proxy v2 enabled - need deployment(pod) annotation - need ha proxy send-proxy-v2 configuration http, https both

--- istio/example/ingress-gateway-success/with-tls/curl.sh
+++ istio/example/ingress-gateway-success/with-tls/curl.sh
@@ -9,9 +9,10 @@ INGRESS_HOST=172.31.39.208
 
 #curl -v --cacert prod.test.dom.crt "https://prod.test.dom:444/"
 
-curl  --cacert prod.test.dom.crt "https://prod.test.dom:444/"
+#curl  --cacert prod.test.dom.crt "https://prod.test.dom:444/"
 #curl --cacert prod.test.dom.crt "https://prod.test.dom:445/"
 
+curl  --cacert prod.test.dom.crt "https://prod.test.dom:444/"
 #curl  "http://prod.test.dom:8080/"
 
 #curl  -HHost:prod.test.dom --resolve "prod.test.dom:$NONE_SECURE_INGRESS_PORT:$INGRESS_HOST" \
