2da36e902c190bcc8ca80aed8e21f8af5b6e9005
Youngju Lee <yjlee@linux.com>
2024-08-16 07:08:56 +0000

    ADD: istio proxy v2 configuration added in istio deployment

--- istio/example/ingress-gateway-success/with-tls/curl.sh
+++ istio/example/ingress-gateway-success/with-tls/curl.sh
@@ -1,7 +1,19 @@
 #!/bin/bash 
 
+NONE_SECURE_INGRESS_PORT=31380
 SECURE_INGRESS_PORT=31390
-INGRESS_HOST=3.38.76.78
+INGRESS_HOST=172.31.39.208
 
-curl  -HHost:prod.test.dom --resolve "prod.test.dom:$SECURE_INGRESS_PORT:$INGRESS_HOST" \
---cacert prod.test.dom.crt "https://prod.test.dom:$SECURE_INGRESS_PORT/"
+#curl  -HHost:prod.test.dom --resolve "prod.test.dom:$SECURE_INGRESS_PORT:$INGRESS_HOST" \
+#--cacert prod.test.dom.crt "https://prod.test.dom:$SECURE_INGRESS_PORT/"
+
+#curl -v --cacert prod.test.dom.crt "https://prod.test.dom:444/"
+
+#curl  --cacert prod.test.dom.crt "https://prod.test.dom:444/"
+#curl --cacert prod.test.dom.crt "https://prod.test.dom:445/"
+
+
+#curl  "http://prod.test.dom:8080/"
+
+curl  -HHost:prod.test.dom --resolve "prod.test.dom:$NONE_SECURE_INGRESS_PORT:$INGRESS_HOST" \
+ "http://prod.test.dom:$NONE_SECURE_INGRESS_PORT/"
